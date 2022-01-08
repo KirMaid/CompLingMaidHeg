50557a3985a7211674873c22e14416a9b5fbdd46
KirMaid <ialavash228@gmail.com>
2022-01-08 22:42:31 +0300

    fix video

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 url = 'https://gorvesti.ru/'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
@@ -12,14 +11,14 @@ urls = []
 dates = []
 titles = []
 texts = []
-video_links = []
+video_link = ""
 news = ""
 for title in links:
     str = title.get('href')
-    if(len(str) > 35 and str[0]=="/" and str[1]!="/"):
-        urls.append(url[:len(url)-1]+str)
-        print(url[:len(url)-1]+str)
-
+    if (len(str) > 35 and str[0] == "/" and str[1] != "/"):
+        urls.append(url[:len(url) - 1] + str)
+        print(url[:len(url) - 1] + str)
+#avova
 for news in urls:
     response = requests.get(news)
     soup = BeautifulSoup(response.text, 'lxml')
@@ -28,15 +27,17 @@ for news in urls:
     dates.append(header.find(class_='dt').text)
     titles.append(header.find(class_='title-block').text)
     texts_default = main.find_all('p')
+    video_link_prev = main.find(class_="video")
+    if video_link_prev is not None:
+        video_link = video_link_prev.find('iframe').src
     for text in texts_default:
-        news += text.text+"\n"
+        news += text.text + "\n"
     texts.append(news)
     print(news)
     print(header.find(class_='dt').text)
 
+# dt = soup.find('.dt').get('href')
+# print(dt)
 
-#dt = soup.find('.dt').get('href')
-#print(dt)
-
-#print(name.text)
-#print(soup.prettify())
+# print(name.text)
+# print(soup.prettify())
