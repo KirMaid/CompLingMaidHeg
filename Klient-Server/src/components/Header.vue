77bba3a7b43eeb728d55ef53ecd7876badfa3738
KirMaid <ialavash228@gmail.com>
2022-01-09 21:32:25 +0300

    Update: fix parser, Client and Server release.

---
+++ Klient-Server/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="container-fluid">
+    <nav class="navbar navbar-light justify-content-center">
+      <a class="navbar-brand" href="https://gorvesti.ru/" target="_blank"
+        >Городские вести
+      </a>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+  setup() {},
+};
+</script>
+<style scoped>
+</style>
--- Klient-Server/src/components/HelloWorld.vue
+++ Klient-Server/src/components/HelloWorld.vue
@@ -1,61 +1,52 @@
 <template>
-  <div class="container hello">
+  <div class="container-fluid">
     <h1>{{ msg }}</h1>
-    <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item" v-for="new_one in news" :key="new_one._id">
+        <p>{{ new_one.date }}</p>
+        <p class="fw-bold">{{ new_one.title }}</p>
+        <p>{{ new_one.content }}</p>
+        <p>{{ new_one.videolink }}</p>
+        <p>
+          <a class="link-primary" target="_blank" v-bind:href="new_one.link">{{
+            new_one.link
+          }}</a>
+        </p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+  data() {
+    return {
+      news: [],
+    };
+  },
+  methods: {
+    getPosts() {
+      //alert("fsdfsdfs");
+      this.$http
+        .get("http://192.168.0.106:3000")
+        .then((response) => {
+          this.news = response.data;
+          //alert(response.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+  mounted() {
+    this.getPosts();
+  },
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
